a14f99bdf0d51577eac968b0bd5e987d948a4c89
gajendra1221102 <02.y.gajender@gmail.com>
2023-04-06 23:48:30 +0530

    new iic

---
+++ iic/iic-main/iic.php
@@ -0,0 +1,391 @@
+<?php
+$title = "Institutions Innovation Council/Startup Cell";
+$description = "Institutions Innovation Council/Startup Cell";
+$keywords = "uiet kuk, uiet kurukshetra, kuk uiet, kuk, University Institute of Engineering & Technology";
+$site_name = "uietkuk.ac.in";
+include 'include/header.php'; ?>
+
+
+
+
+
+<section id="servicesCarousel" class="services services-carousel pt-30 pb-30">
+                <div class="container">
+                    <div class="row" style="height: 100%;">
+                        <div class="col-lg-4">
+                            <div class="admission-tr-page"> <img src="images/dadad.png">
+                                <h2 style="color:#654099">LATEST UPDATES</h2>
+                                <marquee direction="up" scrollamount="2" onMouseOver="this.stop()" onMouseOut="this.start()" height="250">
+                                    <ul class="list-info--1">
+                                        <li><strong><a href="uietkuk.ac.in">KAVACH REGISTRATIONS till April 8th.</a></strong></li>
+                                    </ul>
+                                </marquee>
+                                <div class="power-link">
+                                    <!--<a href="admission-updates.php" class="btn-power "><span>Read More <i class="icon-arrow-right2"></i></span></a>-->
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+            
+            
+            <section id="contact1" class="contact pt-120 pb-110">
+
+                <div class="container">
+          
+                  <div class="row">
+                    
+          
+                    <div class="col-sm-10 col-md-8 col-lg-8">
+
+          
+                            <div class="row">
+          
+                          <div class="col-md-6">
+          
+                          <div class="link-b">
+          
+                            <p><a class="" href="https://ecelluietkuk.web.app/" target="_blank">ecelluietkuk.web.app</a></p></br>
+          
+                            <p><a class="" href="https://linktr.ee/ecelluiet/" target="_blank">Follow us @ E-Cell UIET</a></p></br>
+                            
+                            <p><a class="" href="/pdf-1/CIC policy Doc UIET, KUK.pdf" target="_blank">Institution Innovation and Startup Policy (IISP) </a></p></br>
+                            
+                            <p><a class="" href="https://forms.gle/51cRykspYBr93nqS8" target="_blank">Apply for Community Incubation Centre </a></p></br>
+          
+                        </div>
+                        
+          
+                          </div>
+          
+                          <div class="col-md-6">
+          
+                              <img class="" src="https://www.uietkuk.ac.in/wp-content/uploads/2021/11/ariia_logo-300x65.jpg" alt="">
+          
+                          </div>
+          
+                             </div>
+          
+                            <h5>About IIC</h5>
+          
+                          <p>It's a mandate of MoE's Innovation Cell (MIC) to establish Institution's Innovation Council (IIC) in an institute for systematically fostering the culture of innovation, encourage, inspire and nurture young students by supporting them to work with new ideas and transform them into prototypes. Institution Innovation Council (IIC), UIET, KUK is established to promote innovation through multitudinous modes leading to an innovation promotion eco-system in the campus.</p>
+          
+                          
+          
+                            <h5>Major focus of IIC</h5>
+          
+                          <ul class="word">
+          
+                          <li>To create a vibrant local innovation eco-system and establish a start-up supporting mechanism in the institute.</li>
+          
+                          <li>To prepare institute for Atal Ranking of Institutions on Innovation Achievements (ARIIA) framework.</li>
+          
+                          <li>To establish function ecosystem for scouting ideas and pre-incubation of ideas.</li>
+          
+                          <li>To develop better cognitive ability for technology students.</li>
+          
+                          </ul>
+          
+                          
+          
+                            <h5>Functions of IIC</h5>
+          
+                          <ul class="word">
+          
+                          <li>To conduct various innovation and entrepreneurship-related activities prescribed by Central MIC and self-driven activities in a time bound fashion.</li>
+          
+                          <li>Identify and reward innovations and share success stories.</li>
+          
+                          <li>Organize general measure of enterprising tendency (GET) test for identifying young innovators and entrepreneurs.</li>
+          
+                          <li>Organize periodic workshops/ seminars/ interactions with entrepreneurs, investors, professionals and create a mentor pool for student innovators.</li>
+          
+                          <li>Network with peers and national entrepreneurship development organizations.</li>
+          
+                          <li>To highlight innovative projects carried out by institution's faculty and students.</li>
+          
+                          <li>Organize Hackathons, idea competition, mini-challenges etc. with the involvement of industries.</li>
+          
+                          </ul>
+          
+                          
+          
+                          <div class="link-b">
+          
+                            <p><a class="" href="https://docs.google.com/forms/d/17UXPa4yVprB8TjJ51MRWIQ6Z-7ozvWVXV254G9y5Glo/viewform?edit_requested=true" target="_blank">GET TEST FOR ENTERPRISING POTENTIAL</a></p>
+          
+                        </div>
+          
+                        
+          
+                        
+          
+                          <div class="link-b">
+          
+                            <p><a class="" href="pdf/aria.pdf" target="_blank">ARIIA Report </a></p>
+          
+                        </div>
+          
+                          
+          
+                    </div>          
+          
+                  </div><!-- /.row -->
+          
+                </div><!-- /.container -->
+          
+              </section> 
+
+
+            <section id="about2" class="about about-2 pt-30 pb-30">
+                <div class="container">
+                    <h5>Teacher Coordinators</h5><br><br>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/teacher-1.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Dr. Vishal Ahlawat</h5>
+                                    <p><strong>Convener and IIC President</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/teacher-12.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Dr. Kulvinder Singh
+                                    </h5>
+                                    <p><strong>Incharge, Community Incubation Centre</strong><br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br><br><div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/teacher-7.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">
+                                        Dr. Sunil Nain</h5>
+                                    <p><strong>Vice President</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/teacher-2.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Mr. Archit Sharma</h5>
+                                    <p><strong>IPR Activity Coordinator</strong><br>
+                                      
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br><br><div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/teacher-5.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Mr. Ravinder</h5>
+                                    <p><strong>Internship Activity Coordinator</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/CD.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Dr. Chander Diwakar</h5>
+                                    <p><strong>ARIIA Coordinator and Member</strong><br>
+                                     
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br><br>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/DM.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Dr. Deepak Malik</h5>
+                                    <p><strong>Innovation Activity and Member</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/SC.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Ms. Shivani</h5>
+                                    <p><strong>Social Media and Member</strong><br>
+                                     
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br><br>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/coordinators/SS.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">Ms. Sonia Saini</h5>
+                                    <p><strong>ARIIA Coordinator and Member</strong></p>
+                                </div>
+                            </div>
+                        </div>
+            </section>
+            <section id="about2" class="about about-2 pt-30 pb-30">
+                <div class="container">
+                    <h5>Student Coordinators</h5><br><br>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/core team/himani.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">HIMANI HOODA</h5>
+                                    <p><strong>Team Lead</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/core team/ayushraj.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">
+                                        AYUSH RAJ
+                                    </h5>
+                                    <p><strong>CO-LEAD</strong><br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br><br><div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/core team/sonuyadav.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">
+                                       
+Sonu yadav</h5>
+                                    <p><strong>
+                                        Web developer</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/core team/archiegoel.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">
+                                        Archie goel</h5>
+                                    <p><strong>Social media team
+                                    </strong><br>
+                                      
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br><br><div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="l-left">
+                                <img src="images/core team/umakshi.png" class="logo-loji">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="l-left">
+                                <div class="prof kiji">
+                                    <h5 style="text-align:left">
+                                        Umakshi</h5>
+                                    <p><strong>
+                                        Umakshi</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                    </SECTION>
+
+
+    
+
+        <?php include "include/footer.php" ;?>
